32e75aa1e8f4733d64bd916ba695d2f5a5072b8d
mayankk3115 <47940882+mayankk3115@users.noreply.github.com>
2022-12-25 23:29:10 +0530

    Added LinkedList

---
+++ LinkedList.java
@@ -0,0 +1,7 @@
+package dataStructurePractice.LinkedList;
+
+
+public class LinkedList {
+
+
+}
---
+++ main.java
@@ -0,0 +1,10 @@
+package dataStructurePractice.LinkedList;
+
+import
+
+public class main {
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+    }
+}
